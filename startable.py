0aa3f74da02cfdf06b40bfdcd55ad6bc087a3189
Roy <supti.roy@accenture.com>
2025-09-18 15:40:08 +0530

    Initial commit

---
+++ startable.py
@@ -0,0 +1,7 @@
+j=0
+for i in range(5,0,-1):
+    for j in range(0,5-i+1):
+        print("*",end=" ")
+    print("\n")
+    
+     
---
+++ sumoflist.py
@@ -0,0 +1,5 @@
+l=[2,1,0,1,9,9,8]
+sum=0
+for i in l:
+    sum=sum+i
+print("Sum of list is:",sum)
